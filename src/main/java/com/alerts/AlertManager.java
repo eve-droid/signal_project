71e4f13f739a920336cc9bb95a1075adba7e27d3
Eve <michauxeve@gmail.com>
2024-05-11 18:47:11 +0200

    alert manager and storage

--- src/main/java/com/alerts/AlertManager.java
+++ src/main/java/com/alerts/AlertManager.java
@@ -1,5 +1,34 @@
 package com.alerts;
 
+import java.util.List;
+
 public class AlertManager {
+    private List<Alert> alertList;
+
+    public AlertManager(AlertStorage alertStorage){
+        alertList = alertStorage.getAlerts();
+    }
+
+    public AlertManager(List<Alert> alertList){
+        this.alertList = alertList;
+    }
+
+    public void addAlert(Alert alert){
+        alertList.add(alert);
+    }
     
+    public void resolveAlert(Alert alert){
+        for(int i =0; i<alertList.size(); i++){
+            if(sameAlert(alertList.get(i), alert)){
+                alertList.remove(i);
+            }
+        }
+    } 
+
+    public boolean sameAlert(Alert alert1, Alert alert2){
+        if(alert1.getCondition().equals(alert2.getCondition()) && alert1.getPatientId().equals(alert2.getPatientId()) && alert1.getTimestamp() == alert2.getTimestamp()){
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/com/alerts/AlertStorage.java
+++ src/main/java/com/alerts/AlertStorage.java
@@ -1,5 +1,26 @@
 package com.alerts;
 
-public class AlertStorage {
-    
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Stores all the current triggered alerts of the sytsem
+ * 
+ * it extends AlertManager class to facilitate  
+ */
+public class AlertStorage extends AlertManager {
+    protected List<Alert> AlertList; // Stores alert objects
+
+    /**
+     * Constructs a new instance of DataStorage, initializing the underlying storage
+     * structure.
+     */
+    public AlertStorage() {
+        this.AlertList = new ArrayList<>();
+    }
+
+    public List<Alert> getAlerts(){
+        return AlertList;
+    }
 }
